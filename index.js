78c0d9f9724fcb81989e988fa0aace50dc62b8a7
eggypriambodo <eggypriambodo86@gmail.com>
2023-06-03 07:42:42 +0700

    be

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+
+// Secret key untuk JWT
+const secretKey = "your-user-key";
+
+// Endpoint untuk registrasi
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Hash password menggunakan bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Simpan data pengguna ke database
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// Endpoint untuk login
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Cari pengguna berdasarkan username
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Verifikasi password menggunakan bcrypt
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    // Buat token JWT
+    const token = jwt.sign({ userId: user.id }, secretKey);
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// Middleware untuk memeriksa token JWT
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid token" });
+    }
+
+    req.userId = decoded.userId;
+    next();
+  });
+};
+
+// Endpoint yang memerlukan autentikasi
+app.get("/protected", authenticateToken, (req, res) => {
+  res.json({ message: "Protected endpoint accessed successfully" });
+});
+
+// Jalankan server
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
